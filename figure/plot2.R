3710d0b101eeb2772d0b5385c1ceaba945583f98
Aadi2109S <64983669+Aadi2109S@users.noreply.github.com>
2020-06-06 22:59:07 +0530

    Add files via upload

---
+++ figure/plot2.R
@@ -0,0 +1,19 @@
+library("data.table")
+
+## Getting full dataset
+data_full <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
